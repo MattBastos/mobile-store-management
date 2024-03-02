a9de3752fcfd1bb76754f25d154150f7fdb706aa
MattBastos <matthewsbasttos@gmail.com>
2024-03-02 20:57:42 -0300

    feat: Implements joiSchema to validate data

---
+++ src/utils/validations/joiSchema.js
@@ -0,0 +1,22 @@
+import joi from 'joi';
+
+export const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+}).messages({
+  'string.empty': '{#label} is required!',
+  'string.base': '{#label} needs to be a string!',
+  'string.email': 'Incorrect email!',
+  'string.min': 'Password length must be at least {#limit} characters long',
+});
+
+export const createUserSchema = joi.object({
+  name: joi.string().min(5).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+}).messages({
+  'string.empty': '{#label} is required!',
+  'string.base': '{#label} needs to be a string!',
+  'string.email': 'Incorrect email!',
+  'string.min': 'Password length must be at least {#limit} characters long',
+});
