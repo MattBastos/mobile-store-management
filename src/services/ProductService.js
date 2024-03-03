cf6076f886daa1a3ee8544178f5fa52aebcb355f
MattBastos <matthewsbasttos@gmail.com>
2024-03-03 17:01:04 -0300

    feat: Implements createSimpleProduct method

---
+++ src/services/ProductService.js
@@ -0,0 +1,30 @@
+const ProductFormatter = require('../utils/formatters/ProductFormatter.js');
+const ProductValidator = require('../utils/validations/ProductValidator.js');
+const { Product } = require('../database/models/index.js');
+
+class ProductService {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async createSimpleProduct(product) {
+    const formattedProduct = ProductFormatter.formatSimpleProductObject(product);
+    const productCreationValidation = ProductValidator.validateProductData(formattedProduct);
+
+    if (productCreationValidation) {
+      return {
+        statusCode: productCreationValidation.statusCode,
+        message: productCreationValidation.message,
+      }
+    }
+
+    const newProduct = await this.model.create(formattedProduct);
+
+    return {
+      statusCode: 201,
+      message: newProduct,
+    };
+  }
+}
+
+module.exports = ProductService;
--- src/utils/validations/ProductValidator.js
+++ src/utils/validations/ProductValidator.js
@@ -3,7 +3,9 @@ const { createAndUpdateProductSchema } = require('./joiSchema.js');
 class ProductValidator {
   static validateProductData(product) {
     const { error } = createAndUpdateProductSchema.validate(product);
-    if (error) return { statusCode: 400, message: error.details[0].message }
+    if (error) return { statusCode: 400, message: error.details[0].message };
+
+    return null;
   }
 }
 
