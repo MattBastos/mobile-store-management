877dfc2e5d0fab15a51baaaf6102ea3c5a5c3b77
MattBastos <matthewsbasttos@gmail.com>
2024-03-04 11:01:05 -0300

    feat: Implements Layout component

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,13 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.section`
+  mx-auto
+  my-28
+  flex
+  h-fit
+  w-4/5
+  flex-col
+  items-center
+  justify-start
+  overflow-x-auto
+`;
