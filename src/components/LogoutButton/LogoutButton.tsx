8342d88102a7966127ea1cb2e1c1aac6c290c65e
MattBastos <matthewsbasttos@gmail.com>
2024-03-04 20:09:47 -0300

    feat: Implements LogoutButton component

---
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -0,0 +1,11 @@
+import { SignOut } from "@/components/Icons";
+
+export const LogoutButton = () => {
+  return (
+    <section>
+      <button>
+        <SignOut />
+      </button>
+    </section>
+  );
+};
