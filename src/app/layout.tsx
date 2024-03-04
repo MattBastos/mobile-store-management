ad2a772f92c9e8a3f513ea5263507a8c9c92bd1c
MattBastos <matthewsbasttos@gmail.com>
2024-03-04 11:08:37 -0300

    feat: Add main colors to tailwind config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 export default function RootLayout({ children }: Readonly<LayoutProps>) {
   return (
     <html lang="pt-br" className="scroll-smooth">
-      <body className="flex min-h-screen">
+      <body className="flex min-h-screen bg-background">
         {children}
       </body>
     </html>
