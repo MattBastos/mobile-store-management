bb1ff4fa80a8ef5ac56bca3845ba76ab9c547272
MattBastos <matthewsbasttos@gmail.com>
2024-03-05 17:15:49 -0300

    feat: Implements redirection function on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 import { Layout } from "@/components/Layout";
 import { WelcomeMessage } from "@/components/WelcomeMessage";
 import { ProductTable } from "@/components/Table";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) router.replace('/login');
+  }, []);
+
   return (
     <Layout>
       <WelcomeMessage />
