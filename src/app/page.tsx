b88e1bd22f657449a462fc9d6dd9824e4d19ad10
MattBastos <matthewsbasttos@gmail.com>
2024-03-05 16:05:04 -0300

    feat: Implements WelcomeMessage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Layout } from "@/components/Layout";
+import { WelcomeMessage } from "@/components/WelcomeMessage";
 import { ProductTable } from "@/components/Table";
 
 export default function Home() {
   return (
     <Layout>
+      <WelcomeMessage />
       <ProductTable />
     </Layout>
   );
