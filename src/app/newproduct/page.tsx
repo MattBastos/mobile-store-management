9e5e88131f5fa6a150410c121910c8c768fdaa92
MattBastos <matthewsbasttos@gmail.com>
2024-03-06 09:14:30 -0300

    feat: Redirect user to login page if he does not have a token

--- src/app/newproduct/page.tsx
+++ src/app/newproduct/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 import { Layout } from "@/components/Layout";
 
 export default function NewProduct() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) router.replace('/login');
+  }, []);
+
   return (
     <Layout>
       <h1 className="text-4xl text-white">Criar</h1>
