1eeeff81b81d325e406c8caf72109cd0eeba302b
MattBastos <matthewsbasttos@gmail.com>
2024-03-05 22:42:10 -0300

    refactor: Implements useUpdateProduct custom hook

--- src/components/UpdateProductForm/UpdateProductForm.tsx
+++ src/components/UpdateProductForm/UpdateProductForm.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from "next/navigation";
-
-import { getProductById, updateProduct } from "@/api";
-import { UpdatableProductInfo } from "@/types";
+import { useUpdateProduct } from '@/hooks';
 import { InvalidUserMessage } from '@/components/InvalidUserMessage';
 
 import * as S from './styles';
@@ -14,77 +10,16 @@ type UpdateProductFormProps = {
 }
 
 export const UpdateProductForm = ({ productId }: UpdateProductFormProps) => {
-  const [isUserValid, setIsUserValid] = useState(false);
-  const [message, setMessage] = useState("");
-  const [selectedProduct, setSelectedProduct] = useState<UpdatableProductInfo>({
-    id: 0,
-    name: "",
-    brand: "",
-    model: "",
-    price: 0,
-    color: ""
-  });
-
-  const [formData, setFormData] = useState<UpdatableProductInfo>({
-    id: 0,
-    name: "",
-    brand: "",
-    model: "",
-    price: 0,
-    color: ""
-  });
-
-  const router = useRouter();
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMessage("");
-  
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const isFormDataValid = () => {
-    const { name, brand, model, price, color } = formData;
-    return name.trim() !== "" && brand.trim() !== "" && model.trim() !== "" && price > 0 && color.trim() !== "";
-  };
-
-  const cancelEdition = () => router.push('/');
-
-  const getProduct = async () => {
-    try {
-      const token = localStorage.getItem('token');
-      const response = await getProductById(token, productId);
-  
-      if (response && response.data) {
-        setIsUserValid(true);
-        setSelectedProduct(response.data);
-        setFormData(response.data);
-      }
-    } catch (error) {
-      console.error("Erro ao buscar produto:", error);
-    }
-  }
-
-  const onUpdate = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const token = localStorage.getItem('token');
-      const response = await updateProduct(token, formData);
-
-      if (response?.statusCode === 200) {
-        setMessage("Produto atualizado com sucesso!");
-      } else {
-        setMessage("Você não possui autorização para editar produtos!");
-      }
-    } catch (error) {
-      console.error(`Você não possui autorização para editar produtos: ${error}`);
-    }
-  }
-
-  useEffect(() => {
-    getProduct();
-  }, []);
+  const {
+    isUserValid,
+    selectedProduct,
+    onUpdate,
+    formData,
+    handleChange,
+    message,
+    isFormDataValid,
+    cancelEdition
+  } = useUpdateProduct(productId);
 
   return (
     <>
