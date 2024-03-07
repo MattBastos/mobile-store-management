b4c924eb2ff1e97fb5f6bacdd04eb496b700fc7b
MattBastos <matthewsbasttos@gmail.com>
2024-03-07 12:32:56 -0300

    fix: api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
+
+const port = process.env.API_PORT || 3001;
 
 const app = express();
 
@@ -8,5 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(router);
+
+app.listen(port);
+console.log(`Server is running on port ${port}`);
 
 module.exports = app;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./app.js');
-
-const port = process.env.API_PORT || 3001;
-
-app.listen(port);
-console.log(`Server is running on port ${port}`);
