6a1a22cba808139348ab3a2cfb67847593ba8a56
MattBastos <matthewsbasttos@gmail.com>
2024-03-06 22:32:32 -0300

    feat: Implements navigation on Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,9 @@
+import {
+  DeviceMobile,
+  ListDashes,
+  LinkedinLogo,
+  GithubLogo
+} from '@/components/Icons';
 import * as S from './styles';
 
 const fullYear = new Date().getFullYear();
@@ -13,23 +19,25 @@ export const Footer = () => {
         </S.Section>
 
         <S.Section>
-          <S.Title>Título da Seção</S.Title>
+          <S.Title>Mídias Sociais</S.Title>
 
           <S.List>
             <S.Link
-              href="#"
+              href="https://www.linkedin.com/in/matthewsbastos/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <S.LinkTitle>Link Importante</S.LinkTitle>
+              <LinkedinLogo weight='fill'/>
+              <S.LinkTitle>Linkedin</S.LinkTitle>
             </S.Link>
 
             <S.Link
-              href="#"
+              href="https://github.com/MattBastos"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <S.LinkTitle>Link Importante</S.LinkTitle>
+              <GithubLogo weight='fill' />
+              <S.LinkTitle>Github</S.LinkTitle>
             </S.Link>
           </S.List>
         </S.Section>
@@ -38,12 +46,14 @@ export const Footer = () => {
           <S.Title>Mapa do Site</S.Title>
 
           <S.List>
-            <S.Link href="#">
-              <S.LinkTitle>Link Importante</S.LinkTitle>
+            <S.Link href="/">
+              <ListDashes weight='fill' />
+              <S.LinkTitle>Lista de Produtos</S.LinkTitle>
             </S.Link>
 
-            <S.Link href="#">
-              <S.LinkTitle>Link Importante</S.LinkTitle>
+            <S.Link href="/newproduct">
+              <DeviceMobile weight='fill' />
+              <S.LinkTitle>Criar Produtos</S.LinkTitle>
             </S.Link>
           </S.List>
         </S.Section>
