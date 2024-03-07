3e0dbc3e3e8c9a5724588ba8604f10c49fbf3937
MattBastos <matthewsbasttos@gmail.com>
2024-03-06 23:28:16 -0300

    style: Add responsiveness to login page

--- src/components/Login/FormsContainer/styles.ts
+++ src/components/Login/FormsContainer/styles.ts
@@ -2,14 +2,18 @@ import tw from "tailwind-styled-components";
 
 export const Container = tw.section`
   bg-gray-100
-  p-8
+  p-6
+  md:p-8
   rounded
-  w-[400px]
-  flex flex-col
+  w-[300px]
+  md:w-[400px]
+  flex
+  flex-col
 `;
 
 export const Title = tw.h1`
-  text-2xl
+  text-xl
+  md:text-2xl
   font-bold
   mb-4
   text-secondary
@@ -20,7 +24,8 @@ export const SwitchFormContainer = tw.section`
   flex
   justify-center
   items-center
-  gap-2
+  gap-1
+  md:gap-2
 `;
 
 export const Text = tw.p`
