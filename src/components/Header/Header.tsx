5a1ac53a9cb18e73052ce012ec4f4befc43efe49
MattBastos <matthewsbasttos@gmail.com>
2024-03-06 22:51:27 -0300

    feat: Add navigation to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,24 @@
 import { Logo } from '@/components/Logo';
 import { SignOutButton } from '@/components/SignOutButton';
+import { ListDashes, DeviceMobile } from '@/components/Icons';
 
 import * as S from './styles';
-
 
 export const Header = () => {
   return (
     <S.Container>
-      <section>
+      <section className='flex justify-center items-center gap-4'>
         <Logo />
+
+        <S.Link href="/">
+          <ListDashes size={20} weight='fill' />
+          <S.LinkTitle>Lista de Produtos</S.LinkTitle>
+        </S.Link>
+
+        <S.Link href="/newproduct">
+          <DeviceMobile size={20} weight='fill' />
+          <S.LinkTitle>Criar Produtos</S.LinkTitle>
+        </S.Link>
       </section>
 
       <section className='mr-4'>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,5 @@
+import NextLink from 'next/link';
+
 import tw from 'tailwind-styled-components';
 
 export const Container = tw.header`
@@ -10,4 +12,22 @@ export const Container = tw.header`
   h-24
   w-full
   shadow-xl
+`;
+
+export const Link = tw(NextLink)`
+  flex
+  items-center
+  justify-center
+  gap-1
+  text-gray-200
+  transition-colors
+  duration-300
+  ease-in-out
+  hover:text-primary
+  hover:underline
+`;
+
+export const LinkTitle = tw.h3`
+  text-xl
+  font-semibold
 `;
